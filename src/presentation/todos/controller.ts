0a59177a10d5dd90394b24f2f17d0c58e61f1b69
hector.lopez <hector.lopez@dowjones.com>
2024-07-29 19:25:32 -0500

    adding routes

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,23 +1,33 @@
 import { Response, Request } from "express";
+import { prisma } from "../../data/postgres";
 
 export class TodosController {
     //DI
     constructor() {}
     public getTodos = (req: Request, res: Response) => {
-        res.json({ message: "Hello World" });
+        const todos = prisma.todo.findMany();
+        res.json(todos);
     };
 
     public getTodyById = (req: Request, res: Response) => {
         const id = req.params.id;
-        res.json({ message: `Hello World by id ${id}` });
+        const todo = prisma.todo.findUnique({where:{id:Number(id)}});
+        res.json(todo);
     }
-    public createTodo = (req: Request, res: Response) => {
+     public  createTodo = async (req: Request, res: Response) => {
         const body = req.body;
-        res.json({ message: `Hello World by id ${body}` });
+        const todo = await prisma.todo.create({data:{
+            id: body.id,
+            text: body.text,
+            completedAt: body.completedAt
+        }})
+        res.json(todo);
     }
     public deleteTodo = (req: Request, res: Response) => {
         const id = req.params.id;
-        res.json({ message: `Hello World by id ${id}` });
+        const todo = prisma.todo.delete({where:{id:Number(id)}});
+        res.json(todo);
+   
     }
 
 }
